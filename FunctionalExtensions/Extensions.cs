ff81d3942178202253106d127fb9ed16802c9354
Andrzej Piotrowski <andrzej.piotrowski76@gmail.com>
2021-10-28 19:46:51 +0200

    Init

---
+++ FunctionalExtensions/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FunctionalExtensions
+{
+    public static class Extensions
+    {
+        public static TTo? Map<TFrom, TTo>(this TFrom? input, Func<TFrom, TTo> map) => 
+            input is null 
+                ? default 
+                : map(input);
+
+        public static T? Apply<T>(this T? @this, Action<T> apply)
+        {
+            if (@this is null) 
+                return @this;
+            apply(@this);
+            return @this;
+        }
+    }
+}
